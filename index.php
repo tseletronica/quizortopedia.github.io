af54d18be5bb7cf0416c3b0e71fda6d09ff6b57e
tseletronica <tseletronica@hotmail.com>
2021-05-27 03:24:14 -0300

    fix: correções

--- index.php
+++ index.php
@@ -7,41 +7,41 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
- //Variáveis
- $nome = $_POST['nome'];
- $email = $_POST['email'];
- $mensagem = $_POST['mensagem'];
- $data_envio = date('d/m/Y');
- $hora_envio = date('H:i:s');
+//Variáveis
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$mensagem = $_POST['mensagem'];
+$data_envio = date('d/m/Y');
+$hora_envio = date('H:i:s');
 
 $mail = new PHPMailer(true);
 
 try {
-$mail->SMTPDebug = SMTP::DEBUG_SERVER; //Habilita o modo debug
-$mail->isSMTP();//Habilita para SMTP
-$mail-> Host = 'smtp.gmail.com';//Host do servidor de email
-$mail->SMTPAuth = true;//Habilita autenticação via SMTP
-$mail->Username = 'tiagoserafim2014@gmail.com';//usuario do email
-$mail->Password = '050191Aa';//senha do email
-$mail->Port = 587;//Porta usada pelo servidor SMTP do gmail
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER; //Habilita o modo debug
+    $mail->isSMTP(); //Habilita para SMTP
+    $mail->Host = 'smtp.gmail.com'; //Host do servidor de email
+    $mail->SMTPAuth = true; //Habilita autenticação via SMTP
+    $mail->Username = 'tiagoserafim2014@gmail.com'; //usuario do email
+    $mail->Password = '050191Aa'; //senha do email
+    $mail->Port = 587; //Porta usada pelo servidor SMTP do gmail
 
-$mail->setFrom('tiagoserafim2014@gmail.com');//Email remetente
-$mail->addAddress('tseletronica@hotmail.com');//Email destino
+    $mail->setFrom('tiagoserafim2014@gmail.com'); //Email remetente
+    $mail->addAddress('tseletronica@hotmail.com'); //Email destino
 
-$mail->isHTML(true);//Habilita o modo HTML
-$mail->Subject = 'Assunto do Email';
-$mail->Body = ' <p><b> Nome: </b> $nome</p>
-                <p><b> E-mail: </b> $email </p>
-                <p><b> Mensagem: </b> $menssagem </p>
-                <p> Este e-mail foi enviado em <b> $data_envio </b> às <b> $hora_envio </b> </p>';
-$mail->AltBody = 'chegou a menssagem de Teste '; 
+    $mail->isHTML(true); //Habilita o modo HTML
+    $mail->Subject = 'Assunto do Email';
+    $mail->Body = '
+            "Nome:".$nome "\r\n"
+            "Email:".$email "\r\n"
+            "Mensagem:".$mensagem "\r\n"
+            ';
+    $mail->AltBody = 'chegou a menssagem de Teste ';
 
-if($mail->send()){
-    echo 'Email enviado com sucesso por'.$nome;
-}else {
-    echo 'Email não enviado';
-}
-
+    if ($mail->send()) {
+        echo 'Email enviado com sucesso por' . $nome;
+    } else {
+        echo 'Email não enviado';
+    }
 } catch (Exception $e) {
-        echo "Erro ao enviar menssagem:{$mail -> ErrorInfor}";
+    echo "Erro ao enviar menssagem:{$mail->ErrorInfor}";
 }
