d967b44a74ee2751b773d19010b39909cde9f104
tseletronica <tseletronica@hotmail.comm>
2021-05-31 08:19:56 -0300

    ajustes emergencias

--- index.php
+++ index.php
@@ -39,7 +39,8 @@ try {
     $mail->isHTML(true); //Habilita o modo HTML
     $mail->Subject = $assunto;
     $mail->Body ="<html>
-                                <head> <head>       
+                                <head> <head> 
+                        <h2><b>Nome: </b>$assunto</h2>      
                         <p><b>Nome: </b>$nome</p>
                         <p><b>E-mail: </b>$email</p>
                         <p><b>Mensagem: </b>$mensagem</p>
