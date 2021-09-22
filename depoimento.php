c2a3c52daae3c0d18923e29eda66a17b4c6bdffc
Tiago Serafim <serafim@softbuilder.com.br>
2021-09-22 13:30:49 -0300

    ajustes envio de email

--- depoimento.php
+++ depoimento.php
@@ -1,58 +1,23 @@
 <?php
-require_once('src/PHPMailer.php');
-require_once('src/SMTP.php');
-require_once('src/Exception.php');
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-//Variáveis
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$assunto = $_POST['assuntoSelect'];
-$mensagem = $_POST['mensagem'];
-$data_envio = date('d/m/Y');
-$hora_envio = date('H:i:s');
-
-if ($assunto =='Assunto'){
-        $assunto = 'Dúvida';
-}else{
-    $assunto = $assunto;
-};
-
-$mail->CharSet = 'UTF-8';
-$mail = new PHPMailer(true);
-
-try {
-    //$mail->SMTPDebug = SMTP::DEBUG_SERVER; //Habilita o modo debug
-    $mail->isSMTP(); //Habilita para SMTP
-    $mail->Host = 'smtp.gmail.com'; //Host do servidor de email
-    $mail->SMTPAuth = true; //Habilita autenticação via SMTP
-    $mail->Username = 'tiagoserafim2014@gmail.com'; //usuario do email
-    $mail->Password = '050191Aa'; //senha do email
-    $mail->Port = 587; //Porta usada pelo servidor SMTP do gmail
-
-    $mail->setFrom('tiagoserafim2014@gmail.com'); //Email remetente
-    $mail->addAddress('tseletronica@hotmail.com'); //Email destino
-
-    $mail->isHTML(true); //Habilita o modo HTML
-    $mail->Subject = $assunto;
-    $mail->Body ="<html>
-                        <head> <head> 
-                        <h2><b>Nome: </b>$assunto</h2>      
-                        <p><b>Nome: </b>$nome</p>
-                        <p><b>E-mail: </b>$email</p>
-                        <p><b>Mensagem: </b>$mensagem</p>
-                        <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
-                </html>";
-    $mail->AltBody = 'Caso receba essa menssagem procurar o suporte ';
-
-    if ($mail->send()) {
-        echo "<script> window.location='index.html';alert('$nome, sua mensagem foi enviada com sucesso! Estaremos retornando em breve');</script>";
-    } else {
-        echo 'Email não enviado';
-    }
-} catch (Exception $e) {
-    echo "Erro ao enviar menssagem:{$mail->ErrorInfor}";
+//checka variavel
+if(isset($_POST(['mensagem'])) && !empty($_POST(['mensagem']))){
+    //variaveis
+    $nome = addslashes($_POST(['nome']));
+    $mensagem = addslashes($_POST(['mensagem']));
 }
+
+//
+$to = "contato@quizmed.com.br";
+$subject = "Novo depoimento do usuário registrado!!!";
+$body = "'Nome: '.$nome.'\n'
+        'Mensagem: '.$mensagem.'\n'";
+$header ="From:quizmed@softbuilder.com.br";
+        
+if(mail($to,$subject,$body,$header)){
+    echo("Obrigado, sua mensagem foi enviada com sucesso!");
+}else{
+    echo("sua mensagem não foi enviada,por favor tente novamente!");
+}
+
+?>
