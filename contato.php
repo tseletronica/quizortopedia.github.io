dfa1a4fda686c8c01ac22b9e5a96331da73c6e52
Tiago Serafim <serafim@softbuilder.com.br>
2021-09-22 16:28:06 -0300

    fix

--- contato.php
+++ contato.php
@@ -26,14 +26,14 @@ $mail = new PHPMailer(true);
 
 try {
     $mail->SMTPDebug = SMTP::DEBUG_SERVER; //Habilita o modo debug
-    $mail->SMTPDebug = 3;
+    //$mail->SMTPDebug = 3;
     $mail->SMTPSecure = 'tls';   
     $mail->isSMTP(); //Habilita para SMTP
     $mail->Host = 'mail.softbuilder.com.br'; //Host do servidor de email
     $mail->SMTPAuth = true; //Habilita autenticação via SMTP
     $mail->Username = 'quizmed@softbuilder.com.br'; //usuario do email
     $mail->Password = '@sb4414@'; //senha do email
-    $mail->Port = 587; //Porta usada pelo servidor SMTP do gmail
+    $mail->Port = 587; //Porta usada pelo servidor SMTP
 
     $mail->setFrom('quizmed@softbuilder.com.br'); //Email remetente
     $mail->addAddress('contato@quizmed.com.br'); //Email destino
