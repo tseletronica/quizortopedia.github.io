6220e07a2ed346383b3c6cd7922010303fda42ba
Tiago Serafim <serafim@softbuilder.com.br>
2021-09-22 18:23:44 -0300

    fix

--- depoimento.php
+++ depoimento.php
@@ -1,60 +1,63 @@
 <?php
-require_once('src/PHPMailer.php');
-require_once('src/SMTP.php');
-require_once('src/Exception.php');
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
 //Variáveis
+
 $nome = $_POST['nome'];
-$email = $_POST['email'];
-$assunto = $_POST['assuntoSelect'];
-$mensagem = $_POST['mensagem'];
+$mensagem = $_POST['msg'];
 $data_envio = date('d/m/Y');
 $hora_envio = date('H:i:s');
+// Compo E-mail
 
-if ($assunto =='Assunto'){
-        $assunto = 'Dúvida';
-}else{
-    $assunto = $assunto;
-};
-
-$mail->CharSet = 'UTF-8';
-$mail = new PHPMailer(true);
-
-try {
-    //$mail->SMTPDebug = SMTP::DEBUG_SERVER; //Habilita o modo debug
-   // $mail->SMTPDebug = 3;
-    //$mail->SMTPSecure = 'tls';
-    $mail->isSMTP(); //Habilita para SMTP
-    $mail->Host = 'mail.softbuilder.com.br'; //Host do servidor de email
-    $mail->SMTPAuth = true; //Habilita autenticação via SMTP
-    $mail->Username = 'quizmed@softbuilder.com'; //usuario do email
-    $mail->Password = '@sb4414@'; //senha do email
-    $mail->Port = 587; //Porta usada pelo servidor SMTP do gmail
-
-    $mail->setFrom('quizmed@softbuilder.com.br'); //Email remetente
-    $mail->addAddress('contato@quizmed.com.br'); //Email destino
-
-    $mail->isHTML(true); //Habilita o modo HTML
-    $mail->Subject = $assunto;
-    $mail->Body ="<html>
-                        <head> <head> 
-                        <h2><b>Nome: </b>$assunto</h2>      
-                        <p><b>Nome: </b>$nome</p>
-                        <p><b>E-mail: </b>$email</p>
-                        <p><b>Mensagem: </b>$mensagem</p>
-                        <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
-                </html>";
-    $mail->AltBody = 'Caso receba essa menssagem procurar o suporte ';
-
-    if ($mail->send()) {
-        echo "<script> window.location='index.html';alert('$nome, sua mensagem foi enviada com sucesso! Estaremos retornando em breve');</script>";
-    } else {
-        echo 'Email não enviado';
-    }
-} catch (Exception $e) {
-    echo "Erro ao enviar menssagem:{$mail->ErrorInfor}";
+$arquivo = "
+<style type='text/css'>
+body {
+margin:0px;
+font-family:Verdane;
+font-size:12px;
+color: #666666;
 }
+a{
+color: #666666;
+text-decoration: none;
+}
+a:hover {
+color: #FF0000;
+text-decoration: none;
+}
+</style>
+  <html>
+      <table width='510' border='1' cellpadding='1' cellspacing='1' bgcolor='#CCCCCC'>
+        <tr>      
+        <tr>
+            <td width='500'>Nome:$nome</td>
+        </tr>
+        <tr>
+            <td width='320'>Mensagem:$mensagem</td>
+        </tr>
+        <tr>
+            <td>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></td>
+        </tr>
+      </table>
+  </html>
+";
+
+//enviar
+
+  // emails para quem será enviado o formulário
+  $emailenviar = "contato@quizmed.com.br";
+  $destino = $emailenviar;
+  $assunto = "Contato pelo Site";
+
+  // É necessário indicar que o formato do e-mail é html
+  $headers  = 'MIME-Version: 1.0' . "\r\n";
+      $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+      $headers .= 'From: $nome <$email>';
+  //$headers .= "Bcc: $EmailPadrao\r\n";
+
+  $enviaremail = mail($destino, $assunto, $arquivo, $headers);
+  if($enviaremail){
+  $mgm = "E-MAIL ENVIADO COM SUCESSO! <br> O link será enviado para o e-mail fornecido no formulário";
+  echo " <meta http-equiv='refresh' content='10;URL=contato.php'>";
+  } else {
+  $mgm = "ERRO AO ENVIAR E-MAIL!";
+  echo "";
+  }
