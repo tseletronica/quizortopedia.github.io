3914fb6dea6b779376bee21cba49107ef523ab20
Tiago Serafim <serafim@softbuilder.com.br>
2021-09-22 19:23:42 -0300

    fix

--- PHPMailer_5.2.3/examples/test_mail_basic.php
+++ PHPMailer_5.2.3/examples/test_mail_basic.php
@@ -7,23 +7,28 @@
 <?php
 
 require_once '../class.phpmailer.php';
-$body = " '<h1> teste /h1>'";
+$nome = "teste";
+$Email = "teste@teste";
+$menssagem = "mensagem aqui";
+$body = "Nome ". $nome. "\n".
+        "Email ".$Email. "\n".
+        "Mensagem ". $menssagem. "\n";
 
 $mail = new PHPMailer(true); //defaults to using php "mail()"; the true param means it will throw exceptions on errors, which we need to catch
 
 try {
-  $mail->AddReplyTo('quizmed@softbuilder.com.br', 'First Last');
+  $mail->AddReplyTo('quizmed@softbuilder.com.br', 'quizmed');
   $mail->AddAddress('contato@quizmed.com.br', 'Formulário de Contato');
-  $mail->SetFrom('quizmed@softbuilder.com.br', 'First Last');
-  $mail->AddReplyTo('quizmed@softbuilder.com.br', 'First Last');
+  $mail->SetFrom('quizmed@softbuilder.com.br', 'quizmed');
+  $mail->AddReplyTo('quizmed@softbuilder.com.br', 'quizmed');
   $mail->Subject = 'Novo depoimento do usuário registrado!!!';
   $mail->AltBody = 'Para visualizar a mensagem, use um visualizador de e-mail compatível com HTML'; // optional - MsgHTML will create an alternate automatically
   $mail->msgHTML( $body);
   //$mail->MsgHTML(file_get_contents('contents.html'));
-  $mail->AddAttachment('images/phpmailer.gif');      // attachment
-  $mail->AddAttachment('images/phpmailer_mini.gif'); // attachment
+  //$mail->AddAttachment('images/phpmailer.gif');      // attachment
+  //$mail->AddAttachment('images/phpmailer_mini.gif'); // attachment
   $mail->Send();
-  echo "Message Sent OK\n";
+  echo "Mensagem enviada com sucesso\n";
 } catch (phpmailerException $e) {
   echo $e->errorMessage(); //Pretty error messages from PHPMailer
 } catch (Exception $e) {
