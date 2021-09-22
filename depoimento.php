fc826b9167e4b5a6589545dac366bc584011751a
Tiago Serafim <serafim@softbuilder.com.br>
2021-09-22 16:05:24 -0300

    fix: seta smtp da soft

--- depoimento.php
+++ depoimento.php
@@ -1,23 +1,60 @@
 <?php
+require_once('src/PHPMailer.php');
+require_once('src/SMTP.php');
+require_once('src/Exception.php');
 
-//check variavel
-if(isset($_POST['mensagem']) && !empty($_POST['mensagem'])){
-    //variaveis
-    $nome = addslashes($_POST['nome']);
-    $mensagem = addslashes($_POST['mensagem']);
-}
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-//dados do email
-$to = "tiagoserafim2014@gmail.com";
-$subject = "Novo depoimento do usuário registrado!!!";
-$body = "'Nome: '.$nome.'\n'
-        'Mensagem: '.$mensagem.'\n'";
-$header ="From:serafim@softbuilder.com.br";
-        
-if(mail($to,$subject,$body,$header)){
-    echo("Obrigado, sua mensagem foi enviada com sucesso!");
+//Variáveis
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$assunto = $_POST['assuntoSelect'];
+$mensagem = $_POST['mensagem'];
+$data_envio = date('d/m/Y');
+$hora_envio = date('H:i:s');
+
+if ($assunto =='Assunto'){
+        $assunto = 'Dúvida';
 }else{
-    echo("sua mensagem não foi enviada,por favor tente novamente!");
-}
+    $assunto = $assunto;
+};
 
-?>
+$mail->CharSet = 'UTF-8';
+$mail = new PHPMailer(true);
+
+try {
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER; //Habilita o modo debug
+   // $mail->SMTPDebug = 3;
+    //$mail->SMTPSecure = 'tls';
+    $mail->isSMTP(); //Habilita para SMTP
+    $mail->Host = 'mail.softbuilder.com.br'; //Host do servidor de email
+    $mail->SMTPAuth = true; //Habilita autenticação via SMTP
+    $mail->Username = 'quizmed@softbuilder.com'; //usuario do email
+    $mail->Password = '@sb4414@'; //senha do email
+    $mail->Port = 587; //Porta usada pelo servidor SMTP do gmail
+
+    $mail->setFrom('quizmed@softbuilder.com.br'); //Email remetente
+    $mail->addAddress('contato@quizmed.com.br'); //Email destino
+
+    $mail->isHTML(true); //Habilita o modo HTML
+    $mail->Subject = $assunto;
+    $mail->Body ="<html>
+                        <head> <head> 
+                        <h2><b>Nome: </b>$assunto</h2>      
+                        <p><b>Nome: </b>$nome</p>
+                        <p><b>E-mail: </b>$email</p>
+                        <p><b>Mensagem: </b>$mensagem</p>
+                        <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
+                </html>";
+    $mail->AltBody = 'Caso receba essa menssagem procurar o suporte ';
+
+    if ($mail->send()) {
+        echo "<script> window.location='index.html';alert('$nome, sua mensagem foi enviada com sucesso! Estaremos retornando em breve');</script>";
+    } else {
+        echo 'Email não enviado';
+    }
+} catch (Exception $e) {
+    echo "Erro ao enviar menssagem:{$mail->ErrorInfor}";
+}
